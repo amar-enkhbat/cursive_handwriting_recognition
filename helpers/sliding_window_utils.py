8883a8cfa09ab6fbf1751906e59da020d2a6c040
Amar Enkhbat <enkhbat.amar@gmail.com>
2021-02-01 04:20:06 +0900

    model comparison

--- helpers/sliding_window_utils.py
+++ helpers/sliding_window_utils.py
@@ -2,6 +2,7 @@ import imutils
 import pytesseract
 from custom_dataset_preprocess import rgb2gray, shrink, scale_dataset
 import matplotlib.pyplot as plt
+import numpy as np
 
 def pyramid(image, scale=1.5, minSize=(5, 5)):
     yield image
@@ -36,25 +37,45 @@ def img_preprocessing(img):
     print(img.shape)
     print(img.dtype)
     img = 255.0 - img
-    img = shrink(img, height=56)
+    img = shrink(img, height=28)
     img = scale_dataset(img)
     return img
+
+# def handwriting_detection(img, model, scale=2, stepSize=28, threshold=0.9):
+#     alphabet = "abcdefghijklmnopqrstuvwxyz"
+#     img = img_preprocessing(img)
+    
+#     (winW, winH) = (28, 28)
+#     for resized in pyramid(img, scale):
+#         for (x, y, window) in sliding_window(resized, stepSize=stepSize, windowSize=(winW, winH)):
+#             if window.shape[0] != winH or window.shape[1] != winW:
+#                 continue
+#             clone = resized[y:y+winW, x:x+winH]
+#             y_pred = model.predict(clone.reshape(1, 28, 28, 1))
+#             y_pred_prob = y_pred.max(axis=1)
+#             y_pred = y_pred.argmax(axis=1)
+#             if y_pred_prob > threshold:
+#                 fig, ax = plt.subplots(1)
+#                 ax.imshow(clone, cmap="gray")
+#                 ax.set_title(f"Predicted letter: {alphabet[y_pred[0]]}\nProb: {y_pred_prob}")
+#                 plt.show()
 
 def handwriting_detection(img, model, scale=2, stepSize=28, threshold=0.9):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     img = img_preprocessing(img)
     
-    (winW, winH) = (28, 28)
-    for resized in pyramid(img, scale):
-        for (x, y, window) in sliding_window(resized, stepSize=stepSize, windowSize=(winW, winH)):
-            if window.shape[0] != winH or window.shape[1] != winW:
-                continue
-            clone = resized[y:y+winW, x:x+winH]
-            y_pred = model.predict(clone.reshape(1, 28, 28, 1))
-            y_pred_prob = y_pred.max(axis=1)
-            y_pred = y_pred.argmax(axis=1)
-            if y_pred_prob > threshold:
-                fig, ax = plt.subplots(1)
-                ax.imshow(clone, cmap="gray")
-                ax.set_title(f"Predicted letter: {alphabet[y_pred[0]]}\nProb: {y_pred_prob}")
-                plt.show()
+    (winW, winH) = (14, 28)
+    # for resized in pyramid(img, scale):
+    for (x, y, window) in sliding_window(img, stepSize=stepSize, windowSize=(winW, winH)):
+        if window.shape[0] != winH or window.shape[1] != winW:
+            continue
+        clone = np.ones((28, 28)) * (-1)
+        clone[:, 7:21] = img[y:y+winH, x:x+winW]
+        y_pred = model.predict(clone.reshape(1, 28, 28, 1))
+        y_pred_prob = y_pred.max(axis=1)
+        y_pred = y_pred.argmax(axis=1)
+        if y_pred_prob > threshold:
+            fig, ax = plt.subplots(1)
+            ax.imshow(clone, cmap="gray")
+            ax.set_title(f"Predicted letter: {alphabet[y_pred[0]]}\nProb: {y_pred_prob}")
+            plt.show()
